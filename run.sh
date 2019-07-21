badab7ffaa75632e561b5fa57f2b9f8a54d82087
Yuma Ohgami <yuma@ohgami.jp>
2019-07-21 18:14:09 +0900

    Initial commit.

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+qemu-system-x86_64 \
+	-m 4G \
+	-drive if=pflash,format=raw,readonly,file=$HOME/OVMF/OVMF_CODE.fd \
+	-drive if=pflash,format=raw,file=$HOME/OVMF/OVMF_VARS.fd \
+	-drive dir=fs,driver=vvfat,rw=on
