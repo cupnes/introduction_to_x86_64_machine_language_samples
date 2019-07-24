78cffbe6a63ad636fc06281470c79945e2364412
Yuma Ohgami <yuma@ohgami.jp>
2019-07-24 20:55:27 +0900

    add t2b.sh.

---
+++ t2b.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ue
+
+if [ $# -ne 1 ]; then
+	echo "Usage: $0 FILE" > /dev/stderr
+	exit 1
+fi
+
+FILE=$1
+
+cut -d'#' -f1 ${FILE} | tr ' ' '\n' | tr '\t' '\n' | grep -v '^$' | while read val; do
+	printf "\x${val}"
+done
