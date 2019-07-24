7e9c91337e2a7c8e381a759ab07a1edd4c52a000
Yuma Ohgami <yuma@ohgami.jp>
2019-07-24 20:54:11 +0900

    run.sh: support extra arguments.

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ qemu-system-x86_64 \
 	-m 4G \
 	-drive if=pflash,format=raw,readonly,file=$HOME/OVMF/OVMF_CODE.fd \
 	-drive if=pflash,format=raw,file=$HOME/OVMF/OVMF_VARS.fd \
-	-drive dir=fs,driver=vvfat,rw=on
+	-drive dir=fs,driver=vvfat,rw=on \
+	$@
