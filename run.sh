1b9c2f35a5bb1de4784f1f956863e6a38b7c35b3
Yuma Ohgami <yuma@ohgami.jp>
2019-07-25 23:25:04 +0900

    run.sh: update.

--- run.sh
+++ run.sh
@@ -1,7 +1,19 @@
-#!/bin/sh
+#!/bin/bash
+
+set -ue
+# set -uex
+
+if [ $# -lt 1 ]; then
+	echo "usage: $0 FILESYSTEM_PATH [QEMU_ADDITIONAL_OPTION ...]" > /dev/stderr
+	exit 1
+fi
+
+fs_path=$1
+shift
+
 qemu-system-x86_64 \
 	-m 4G \
 	-drive if=pflash,format=raw,readonly,file=$HOME/OVMF/OVMF_CODE.fd \
 	-drive if=pflash,format=raw,file=$HOME/OVMF/OVMF_VARS.fd \
-	-drive dir=fs,driver=vvfat,rw=on \
+	-drive dir=${fs_path},driver=vvfat,rw=on \
 	$@
